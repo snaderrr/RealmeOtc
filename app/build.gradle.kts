934e89a9edecb93afb54f23c8a829da29cdadaa0
snaderrr <snade.wts@yandex.ru>
2026-09-25 11:42:42 +0500

    Fix: RTSP-Server 1.3.0 for SDK 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
 
     implementation("com.github.pedroSG94.RootEncoder:library:2.5.5")
-    implementation("com.github.pedroSG94:RTSP-Server:1.4.2")
+    implementation("com.github.pedroSG94:RTSP-Server:1.3.0")
 
     implementation("androidx.media3:media3-exoplayer:1.3.1")
     implementation("androidx.media3:media3-exoplayer-rtsp:1.3.1")

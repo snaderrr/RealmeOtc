0a241a2aa5714bf8cfa7e83c277e78b2300b89f6
snaderrr <snade.wts@yandex.ru>
2026-09-25 11:21:43 +0500

    Fix: update to AGP 9.0, SDK 37, Gradle 9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.realme.otc"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.realme.otc"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
     }

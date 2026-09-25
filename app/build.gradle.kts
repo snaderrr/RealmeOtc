b059369dd998d03e74f060dc4af39710249ee78b
snaderrr <snade.wts@yandex.ru>
2026-09-25 11:31:59 +0500

    Fix: correct RootEncoder and RTSP-Server dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.realme.otc"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.realme.otc"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
@@ -32,8 +32,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
 
-    implementation("com.github.pedroSG94.RootEncoder:library:2.4.0")
-    implementation("com.github.pedroSG94:RTSP-Server:1.1.5")
+    implementation("com.github.pedroSG94.RootEncoder:library:2.8.0")
+    implementation("com.github.pedroSG94:RTSP-Server:1.3.3")
 
     implementation("androidx.media3:media3-exoplayer:1.3.1")
     implementation("androidx.media3:media3-exoplayer-rtsp:1.3.1")
